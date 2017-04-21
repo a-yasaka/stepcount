69155e55f17ad4e1396e0b07812f9334a910a52a
A. Yasaka <a-yasaka@nri.co.jp>
2017-04-21 15:14:18 +0900

    dependency defined

--- src/main/java/stepcount/StepCount.java
+++ src/main/java/stepcount/StepCount.java
@@ -1,9 +1,19 @@
 package stepcount;
 
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.Executor;
+
 public class StepCount {
 
-	public static void main(String args[]){
+	public static void main(String args[]) throws ExecuteException, IOException{
 		String basedir=args[0];
-		String target=args[1];
+		final String cmdstr="dir /B /S >"+basedir+">"+basedir+"\\files.txt";
+		CommandLine cmd=CommandLine.parse(cmdstr);
+		Executor exec=new DefaultExecutor();
+		exec.execute(cmd);
 	}
 }
